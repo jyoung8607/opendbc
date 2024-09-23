8df64ee0b8d1aa1df19bb00129fecb6d133b4cb7
Jason Young <jyoung8607@gmail.com>
2024-09-23 18:23:04 -0400

    who even likes single tick

--- opendbc/car/structs.py
+++ opendbc/car/structs.py
@@ -88,7 +88,7 @@ class CarState:
 
   # gear
   gearShifter: 'CarState.GearShifter' = field(default_factory=lambda: CarState.GearShifter.unknown)
-  driveMode: `CarState.DriveMode` = field(default_factory=lambda: CarState.DriveMode.unknown)
+  driveMode: 'CarState.DriveMode' = field(default_factory=lambda: CarState.DriveMode.unknown)
 
   # button presses
   buttonEvents: list['CarState.ButtonEvent'] = auto_field()
